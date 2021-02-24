9d4c37f64b095f0dbac49d304704d579366ab32d
Michael Roche <m.t.roche1996@gmail.com>
2021-02-24 22:51:53 +0000

    Created functions for the different processes

--- review_scraper.py
+++ review_scraper.py
@@ -11,29 +11,31 @@ from collections import defaultdict
 import pandas as pd
 import requests
 
-# Scrape time
-start = time.time()
+from movie_scraper import get_next_movie 
 
 
-### --- Scraping the movie review pages for our chosen movie --- ###
+### --- get_movie_info function provides the movie id and movie name --- ###
 
 
-# Defining the URL and requesting the HTML documentation
-url      = 'https://www.rottentomatoes.com/m/clockwork_orange/reviews?type=user'
-response = requests.get(url)
+def get_movie_info(url):
+    # Requesting the HTML documentation
+    response = requests.get(url)
 
-# Searching the HTML doc to extract the movie_id
-html_data  = json.loads(re.search('movieReview\s=\s(.*);', response.text).group(1))
-movie_id   = html_data['movieId']
-# Extracting movie_name for file saving
-movie_name = html_data['title']
-movie_name = movie_name.lower().replace(' ','_')
+    # Searching the HTML doc to extract the movie_id
+    html_data  = json.loads(re.search('movieReview\s=\s(.*);', response.text).group(1))
+    movie_id   = html_data['movieId']
+    # Extracting movie_name for file saving
+    movie_name = html_data['title']
+    movie_name = movie_name.lower().replace(' ','_')
+
+    # Returning movie_id and movie_name
+    return movie_id, movie_name
 
 
-### --- Defining function to flick through the review pages --- ###
+### --- get_reviews function flicks through the review pages --- ###
 
 
-def get_reviews(endCursor):
+def get_review_page(endCursor, movie_id):
     r = requests.get(f'https://www.rottentomatoes.com/napi/movie/{movie_id}/reviews/user',
     params = {
         "direction": "next",
@@ -43,74 +45,90 @@ def get_reviews(endCursor):
     return r.json()
 
 
-### --- Using get_reviews to loop through thousands of review pages for our chosen movie --- ###
+### --- get_all_reviews function uses get_review_page to loop through thousands of review pages for our chosen movie --- ###
 
 
-# Empty reviews list and result dictionary
-reviews = []
-result  = {}
-   
-# Looping over review pages until final page or once 5,000 pages scraped
-i = 0
-while True:
-    # Random no. generator created to pause code and prevent overloading servers
-    # 0 < r < 1 
-    r = random.random()
-    time.sleep(2*r) #[s]
+def get_all_reviews(movie_id):
+    # Empty reviews list and result dictionary
+    reviews = []
+    result  = {}
+    
+    i = 0
+    while True:
+        try:
+            # Random no. generator created to pause code and prevent overloading servers
+            r = random.random()
+            time.sleep(2*r) #[s]
 
-    # Gathering all movie review information into list: reviews
-    result = get_reviews(result['pageInfo']['endCursor'] if i != 0  else '')
-    reviews.extend([t for t in result['reviews']])
+            # Gathering all movie review information into list: reviews
+            result = get_review_page(result['pageInfo']['endCursor'] if i != 0  else '', movie_id)
+            reviews.extend([t for t in result['reviews']])
 
-    # Stopping loop at final page or after 5,000 pages
-    if result['pageInfo']['hasNextPage']==False:
-        break
-    elif i > 5000:
-        break
+            # Stopping loop at final page or after 5,000 pages
+            if result['pageInfo']['hasNextPage']==False:
+                break
+            elif i > 5000:
+                break
 
-    # Pausing for a minute after 1,000 reviews (100 pages)
-    i += 1
-    if i%100==0:
-        time.sleep(60)
-
-
-### --- Parsing the relevant data and putting it into a dataframe --- ###
-
-
-# Empty data dictionary
-data = defaultdict(list)
-
-# Finding reviewers who have user id's 
-users_all = [reviews[i]['user']['userId'] for i in range(len(reviews))]
-idx       = [i for i in range(len(users_all)) if len(users_all[i]) == 9]
-users     = [reviews[i]['user']['userId'] for i in idx]
-data['user'].extend(users)
-
-# Verified
-super_reviewer = [reviews[i]['isSuperReviewer'] for i in idx]
-super_reviewer = [int(x) for x in super_reviewer]
-data['super_reviewer'].extend(super_reviewer)
-
-# Profanity
-profanity = [reviews[i]['hasProfanity'] for i in idx]
-profanity = [int(x) for x in profanity]
-data['profanity'].extend(profanity)
-
-# Written Review
-data['review'].extend([reviews[i]['review'] for i in idx])
-
-# Star rating
-star_rating = [reviews[i]['rating'] for i in idx]
-star_rating = [float(x.replace('STAR_','').replace('_','.')) for x in star_rating]
-data['rating'].extend(star_rating)
-
-# Creating dataframe of reviews
-df = pd.DataFrame(data)
-df.to_pickle(f'./review_dfs/{movie_name}.pkl')
+            # Pausing for a minute after 10,000 reviews (100 pages)
+            i += 1
+            if i%1000==0:
+                time.sleep(60)
+        
+        except requests.exceptions.RequestException:
+            print('Wifi out, waiting...')
+            time.sleep(30)
+    
+    return reviews
 
 
-### --- Testing run time for movies --- ###
+### --- Parsing function that obtains relevant data and puts it into a dataframe --- ###
 
 
-end = time.time()
-print(f'Time to run:{(start-end)/3600:.2f} hrs')
+def parse_reviews(movie_id, movie_name):
+    # Getting all reviews
+    reviews = get_all_reviews(movie_id)
+
+    # Empty data dictionary
+    data = defaultdict(list)
+
+    # Finding reviewers who have user id's 
+    users_all = [reviews[i]['user']['userId'] for i in range(len(reviews))]
+    idx       = [i for i in range(len(users_all)) if len(users_all[i]) == 9]
+    users     = [reviews[i]['user']['userId'] for i in idx]
+    data['user'].extend(users)
+
+    # Verified
+    super_reviewer = [reviews[i]['isSuperReviewer'] for i in idx]
+    super_reviewer = [int(x) for x in super_reviewer]
+    data['super_reviewer'].extend(super_reviewer)
+
+    # Profanity
+    profanity = [reviews[i]['hasProfanity'] for i in idx]
+    profanity = [int(x) for x in profanity]
+    data['profanity'].extend(profanity)
+
+    # Written Review
+    data['review'].extend([reviews[i]['review'] for i in idx])
+
+    # Star rating
+    star_rating = [reviews[i]['rating'] for i in idx]
+    star_rating = [float(x.replace('STAR_','').replace('_','.')) for x in star_rating]
+    data['rating'].extend(star_rating)
+
+    # Creating dataframe of reviews
+    df = pd.DataFrame(data)
+    df.to_pickle(f'./review_dfs/{movie_name}.pkl')
+    return df
+
+
+### --- Run Code --- ###
+
+
+if __name__ == '__main__':
+    start = time.time()
+    url = 'https://www.rottentomatoes.com/m/cannibal_holocaust/reviews?type=user'
+    movie_id, movie_name = get_movie_info(url)
+    parse_reviews(movie_id, movie_name)
+    end = time.time()
+    print(f'Time to run:{(start-end)/3600:.2f} hrs')
