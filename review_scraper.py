e5f4b9f392166109185a56a63f88317e0ffae61a
Michael Roche <m.t.roche1996@gmail.com>
2021-02-26 23:53:13 +0000

    review_scraper set to scrape several movies

--- review_scraper.py
+++ review_scraper.py
@@ -1,23 +1,20 @@
 ### --- In this file, we scrape the rotten tomatoes website for user reviews --- ###
 ### --- Importing modules --- ###
-
-
 import json
 import random
 import re
 import time
 from collections import defaultdict
 
+import numpy as np
 import pandas as pd
 import requests
 
-from movie_scraper import get_next_movie 
 from api_call import search_for_movie
+from movie_scraper import get_next_movie
 
 
 ### --- get_movie_info function provides the movie id and movie name --- ###
-
-
 def get_movie_info(url_id):
     # Requesting the HTML documentation
     response = requests.get(f'https://www.rottentomatoes.com/{url_id}/reviews?type=user')
@@ -29,15 +26,11 @@ def get_movie_info(url_id):
     movie_name = html_data['title']
     movie_name = movie_name.lower().replace(' ','_')
 
-    print(f'Saving name is: {movie_name}')
-
     # Returning movie_id and movie_name
     return movie_name, movie_id
 
 
 ### --- get_reviews function flicks through the review pages --- ###
-
-
 def get_review_page(endCursor, movie_id):
     r = requests.get(f'https://www.rottentomatoes.com/napi/movie/{movie_id}/reviews/user',
     params = {
@@ -49,10 +42,7 @@ def get_review_page(endCursor, movie_id):
 
 
 ### --- get_all_reviews function uses get_review_page to loop through thousands of review pages for our chosen movie --- ###
-
-
 def get_all_reviews(movie_id):
-    # Empty reviews list and result dictionary
     reviews = []
     result  = {}
     
@@ -60,19 +50,16 @@ def get_all_reviews(movie_id):
     while True:
         try:
             # Random no. generator created to pause code and prevent overloading servers
-            r = random.random()
-            time.sleep(2*r) #[s]
+            r = np.abs(np.random.randn()/3 + 1)
+            time.sleep(r) #[s]
 
             # Gathering all movie review information into list: reviews
             result = get_review_page(result['pageInfo']['endCursor'] if i != 0  else '', movie_id)
             reviews.extend([t for t in result['reviews']])
 
             # Stopping loop at final page or after 5,000 pages
-            if result['pageInfo']['hasNextPage']==False:
+            if result['pageInfo']['hasNextPage']==False or i > 4999:
                 break
-            elif i > 5000:
-                break
-
             # Pausing for a minute after 10,000 reviews (100 pages)
             i += 1
             if i%1000==0:
@@ -86,38 +73,60 @@ def get_all_reviews(movie_id):
 
 
 ### --- Parsing function that obtains relevant data and puts it into a dataframe --- ###
-
-
-def parse_reviews(save_name, movie_id):
-    # Getting all reviews
+def parse_reviews(save_name, movie_id, movie_year, meter_score):
     reviews = get_all_reviews(movie_id)
+    N = len(reviews)
 
-    # Empty data dictionary
-    data = defaultdict(list)
+    # data = defaultdict(list)
+    data = {}
 
-    # Finding reviewers who have user id's 
-    users_all = [reviews[i]['user']['userId'] for i in range(len(reviews))]
-    idx       = [i for i in range(len(users_all)) if len(users_all[i]) == 9]
-    users     = [reviews[i]['user']['userId'] for i in idx]
-    data['user'].extend(users)
+    # Movie name
+    data['movie_name'] = [save_name]*N
+    # Movie year
+    data['movie_year'] = [movie_year]*N
+    # Meter score
+    data['meter_score'] = [meter_score]*N
+
+    # User id
+    users_all = [reviews[i]['user']['userId'] for i in range(N)]
+    users     = [reviews[i]['user']['userId'] 
+                if len(users_all[i]) == 9 
+                else np.nan 
+                for i in range(N)]
+    data['user'] = users
+
+    # Review date
+    date = [reviews[i]['updateDate'][:9] for i in range(N)]
+    data['post_date'] = date
 
     # Verified
-    super_reviewer = [reviews[i]['isSuperReviewer'] for i in idx]
+    verified = [reviews[i]['isVerified'] for i in range(N)]
+    verified = [int(x) for x in verified]
+    data['verified'] = verified
+
+    # Super reviewer
+    super_reviewer = [reviews[i]['isSuperReviewer'] for i in range(N)]
     super_reviewer = [int(x) for x in super_reviewer]
-    data['super_reviewer'].extend(super_reviewer)
+    data['super_reviewer'] = super_reviewer
+
+    # Spoilers
+    spoilers = [reviews[i]['hasSpoilers'] for i in range(N)]
+    spoilers = [int(x) for x in spoilers]
+    data['spoilers'] = spoilers
 
     # Profanity
-    profanity = [reviews[i]['hasProfanity'] for i in idx]
+    profanity = [reviews[i]['hasProfanity'] for i in range(N)]
     profanity = [int(x) for x in profanity]
-    data['profanity'].extend(profanity)
+    data['profanity'] = profanity
 
     # Written Review
-    data['review'].extend([reviews[i]['review'] for i in idx])
+    review         = [reviews[i]['review'] for i in range(N)]
+    data['review'] = review
 
     # Star rating
-    star_rating = [reviews[i]['rating'] for i in idx]
-    star_rating = [float(x.replace('STAR_','').replace('_','.')) for x in star_rating]
-    data['rating'].extend(star_rating)
+    star_rating    = [reviews[i]['rating'] for i in range(N)]
+    star_rating    = [float(x.replace('STAR_','').replace('_','.')) for x in star_rating]
+    data['rating'] = star_rating
 
     # Creating dataframe of reviews
     df = pd.DataFrame(data)
@@ -126,16 +135,14 @@ def parse_reviews(save_name, movie_id):
 
 
 ### --- Run Code --- ###
-
-
 if __name__ == '__main__':
     start = time.time()
-
-    # Calling functions
-    search_name, movie_year = get_next_movie()
-    movie_name, url_id      = search_for_movie(search_name, int(movie_year))
-    save_name, movie_id     = get_movie_info(url_id)
-    parse_reviews(save_name, movie_id)
+    for i in range(4):
+        # Calling functions
+        search_name, movie_year         = get_next_movie()
+        movie_name, url_id, meter_score = search_for_movie(search_name, movie_year)
+        save_name, movie_id             = get_movie_info(url_id)
+        parse_reviews(save_name, movie_id, movie_year, meter_score)
 
     end = time.time()
     print(f'Time to run:{(start-end)/3600:.2f} hrs')
